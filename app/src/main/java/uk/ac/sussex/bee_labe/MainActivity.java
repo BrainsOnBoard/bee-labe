75b163a444de5aa4fa0b456e4797ccf8e54b47f4
Alex Dewar <a.dewar@sussex.ac.uk>
2017-09-15 16:38:04 +0100

    log phone's attitude and display data on screen

--- app/src/main/java/uk/ac/sussex/bee_labe/MainActivity.java
+++ app/src/main/java/uk/ac/sussex/bee_labe/MainActivity.java
@@ -1,11 +1,14 @@
 package uk.ac.sussex.bee_labe;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.hardware.*;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener, View.OnClickListener {
     private static final int SENSOR_DELAY = 1000000; // µs
@@ -13,10 +16,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager mSensorManager;
     private Sensor mAccSensor, mMagSensor;
     private float[] mGravity, mGeomagnetic;
-    private float pitch, roll, yaw;
+    private float yaw, pitch, roll;
     private boolean isRecording = false;
     private Button recButton;
     private TextView pitchView, rollView, yawView;
+    private ArrayList<Attitude> data = new ArrayList(100);
+    private boolean isPaused;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,24 +42,54 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     protected void onResume() {
         super.onResume();
+        isPaused = false;
         mSensorManager.registerListener(this, mAccSensor, SENSOR_DELAY, SENSOR_DELAY);
         mSensorManager.registerListener(this, mMagSensor, SENSOR_DELAY, SENSOR_DELAY);
     }
 
     protected void onPause() {
         super.onPause();
-        mSensorManager.unregisterListener(this);
+        isPaused = true;
+
+        if (!isRecording) {
+            mSensorManager.unregisterListener(this);
+        }
     }
 
     @Override
     public void onClick(View view) {
         if(isRecording) {
-            recButton.setText("Start Recording");
-            isRecording = false;
+            stopRecording();
         } else {
-            recButton.setText("Stop Recording");
-            isRecording = true;
+            startRecording();
         }
+    }
+
+    private void startRecording() {
+        isRecording = true;
+        recButton.setText("Stop Recording");
+    }
+
+    private void stopRecording() {
+        isRecording = false;
+        recButton.setText("Start Recording");
+
+        String dString = "";
+        for (int i = 0; i < data.size(); i++) {
+            dString += data.get(i) + "\n";
+        }
+        data.clear();
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Data");
+        dialog.setMessage(dString);
+        dialog.setPositiveButton(" OK ", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+
+            }
+        });
+        dialog.show();
     }
 
     @Override
@@ -74,14 +109,36 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             if (success) {
                 float orientation[] = new float[3];
                 SensorManager.getOrientation(R, orientation);
-                yaw = orientation[0]; // orientation contains: azimuth, pitch and roll
-                pitch = -orientation[1];
-                roll = orientation[2];
 
-                pitchView.setText(String.format("Pitch: %.2f°", Math.toDegrees(pitch)));
-                rollView.setText(String.format("Roll: %.2f°", Math.toDegrees(roll)));
-                yawView.setText(String.format("Yaw: %.2f°", Math.toDegrees(yaw)));
+                if (!isPaused) {
+                    yaw = orientation[0];
+                    pitch = -orientation[1];
+                    roll = orientation[2];
+
+                    pitchView.setText(String.format("Pitch: %.2f°", Math.toDegrees(pitch)));
+                    rollView.setText(String.format("Roll: %.2f°", Math.toDegrees(roll)));
+                    yawView.setText(String.format("Yaw: %.2f°", Math.toDegrees(yaw)));
+                }
+
+                if (isRecording) {
+                    data.add(new Attitude(orientation));
+                }
             }
+        }
+    }
+
+    class Attitude {
+        public float yaw, pitch, roll;
+
+        public Attitude(float[] orient) {
+            yaw = orient[0];
+            pitch = -orient[1];
+            roll = orient[2];
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%.2f, %.2f, %.2f", this.yaw, this.pitch, this.roll);
         }
     }
 }
